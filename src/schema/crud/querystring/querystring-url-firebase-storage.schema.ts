c92703c2e4c1496f8353e80a8aa28435e742ae87
denis23x <damage.23x@gmail.com>
2024-05-17 12:23:33 +0700

    chore: update

--- src/schema/crud/querystring/querystring-url-firebase-storage.schema.ts
+++ src/schema/crud/querystring/querystring-url-firebase-storage.schema.ts
@@ -7,7 +7,7 @@ export const querystringUrlFirebaseStorageSchema: Record<string, any> = {
   properties: {
     url: {
       type: 'string',
-      default: 'https://firebasestorage.googleapis.com/v0/b/takabase-local.appspot.com/o/seed/1.webp?alt=media',
+      default: `https://firebasestorage.googleapis.com/v0/b/${JSON.parse(process.env.APP_SERVICE_ACCOUNT).project_id}.appspot.com/o/seed/1.webp`,
       format: 'uri',
       pattern: '^https:\\/\\/firebasestorage\\.googleapis\\.com(:\\d+)?(\\/[-a-zA-Z0-9@:%._+~#=]*)*(\\?[;&amp;a-zA-Z0-9@:%_+.~#?&amp;\\/=]*)?(#[a-zA-Z0-9_@:%#?&amp;\\/=+-]*)?$'
     }
